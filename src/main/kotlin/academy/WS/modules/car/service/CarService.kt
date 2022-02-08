e91c50bbdbe9c2e5005c1094b07af2ce15d13ca2
Luccas Pereira <Luccas.lpn@outlook.com>
2022-02-07 23:51:24 -0300

    Delete, FindById, FindAll

--- src/main/kotlin/academy/WS/modules/car/service/CarService.kt
+++ src/main/kotlin/academy/WS/modules/car/service/CarService.kt
@@ -4,8 +4,10 @@ import academy.WS.modules.car.domain.Car
 import academy.WS.modules.car.domain.CarPost
 import academy.WS.modules.car.mapper.CarMapper
 import academy.WS.modules.car.repository.CarRepository
+import academy.WS.modules.factory.domain.Factory
 import academy.WS.modules.factory.service.FactoryService
 import org.springframework.stereotype.Service
+import javax.xml.bind.ValidationException
 
 @Service
 class CarService(val carRepository: CarRepository, val factoryService: FactoryService){
@@ -20,6 +22,20 @@ class CarService(val carRepository: CarRepository, val factoryService: FactorySe
         }
         return carRepository.save(car)
     }
+    
+    fun delete(id: Int){
+        carRepository.delete(findByIdOrThrowBadRequestException(id))
+    }
+
+    fun findByIdOrThrowBadRequestException(id: Int): Car{
+        return carRepository.findById(id)
+            .orElseThrow { ValidationException("Car not Found") }
+    }
+
+    fun findAll():MutableList<Car>{
+        return carRepository.findAll()
+    }
+
 
 
 
